0701174670ce16f185a703accdfc06aef64f8884
Aashish Op <ashishguptaop195@gmail.com>
2025-02-24 23:03:58 +0530

    my backend

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+app.post('/teas', (req, res) => {
+
+    const {name, price } = req.body
+    const newTea = {id: nextId++, name, price}
+    teaData.push(newTea)
+    res.status (201).send(newTea)
+})
+
+app.get('/teas', (req, res) => {
+    res.status(200).send(teaData)
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/`)
+})
